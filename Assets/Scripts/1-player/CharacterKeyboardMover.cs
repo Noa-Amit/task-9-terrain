754ed24402fc14bcf5dc8403774666582fb482a5
Amit Ran <57709369+amitRan109@users.noreply.github.com>
2020-12-28 14:19:21 +0200

    Initial commit

---
+++ Assets/Scripts/1-player/CharacterKeyboardMover.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+/**
+ * This component moves a player controlled with a CharacterController using the keyboard.
+ */
+[RequireComponent(typeof(CharacterController))]
+public class CharacterKeyboardMover: MonoBehaviour {
+    [Tooltip("Speed of player keyboard-movement, in meters/second")]
+    [SerializeField] float _speed = 3.5f;
+    [SerializeField] float _gravity = 9.81f;
+    [SerializeField] float _jump_pulse = 10f;
+
+    private CharacterController _cc;
+    void Start() {
+        _cc = GetComponent<CharacterController>();
+    }
+
+    Vector3 velocity;
+
+    void Update()  {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        //if (x == 0 && z == 0) return;
+        velocity.x = x * _speed;
+        velocity.z = z * _speed;
+        if (Input.GetButtonDown("Jump")) {
+            velocity = new Vector3(Input.GetAxis("Horizontal"), _jump_pulse, Input.GetAxis("Vertical"));
+        }
+        if (!_cc.isGrounded) {
+            velocity.y -= _gravity*Time.deltaTime;
+        }
+        velocity = transform.TransformDirection(velocity);
+        _cc.Move(velocity * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/1-player/RayCast.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCast : MonoBehaviour
+{
+	private GameObject moveObject = null;
+	bool isRay = false;
+	int seconds = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	     if(Input.GetKeyDown(KeyCode.E)) {
+			 Ray rayOrigin = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+			 RaycastHit hitInfo;
+			 int layer_mask = LayerMask.GetMask("cubes");
+			 if (Physics.Raycast(rayOrigin, out hitInfo,Mathf.Infinity, layer_mask)) {
+			 	Debug.Log("ray hits: "+hitInfo.collider.gameObject.name);
+			 	moveObject = hitInfo.collider.gameObject;
+			 	isRay = true;
+			 	seconds = 0;
+		 	 }
+	 	 }
+	 	 if (isRay){
+		 	 if(Input.GetKeyDown(KeyCode.G)) {
+		 	 	moveObject.transform.position += new Vector3(-1f,0f,0f);
+		 	 }
+		 	  if(Input.GetKeyDown(KeyCode.J)) {
+		 	 	moveObject.transform.position += new Vector3(1f,0f,0f);
+		 	 }
+		 	  if(Input.GetKeyDown(KeyCode.Y)) {
+		 	 	moveObject.transform.position += new Vector3(0f,0f,1f);
+		 	 }
+		 	  if(Input.GetKeyDown(KeyCode.H)) {
+		 	 	moveObject.transform.position += new Vector3(0f,0f,-1f);
+		 	 }
+		 	 seconds++;
+	 	 }
+	 	 if(seconds >= 100) {
+	 	 	seconds = 0;
+	 	 	isRay = false; 
+	 	 	moveObject = null;
+	 	 }
+
+    }
+}
